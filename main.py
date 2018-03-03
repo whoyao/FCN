d165f304065c8fc95661efee71e96fd6c841954a
whoyao <qsct9501@126.com>
2018-03-03 11:28:11 +0800

    -'

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
+    # FIXME: make sure your code is right
     #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
@@ -53,7 +53,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
+    # FIXME: make sure your code is right
     # manipulation for layer7
     conv_1x1_layer7 = tf.layers.conv2d(vgg_layer7_out, num_classes, strides=(1,1), padding='same',
                                 kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
@@ -91,7 +91,9 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
+    # FIXME: make sure your code is right
+    logits = tf.reshape(nn_last_layer,(-1,num_classes))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, correct_label))
     return None, None, None
 tests.test_optimize(optimize)
 
