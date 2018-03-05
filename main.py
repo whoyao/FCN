bd8eadc89343ae3d9c4ef587b210db49b3f2aab9
hooyao <qsct9501@126.com>
2018-03-05 19:22:14 +0800

    -

--- main.py
+++ main.py
@@ -56,21 +56,27 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     # FIXME: make sure your code is right
     # manipulation for layer7
     conv_1x1_layer7 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, strides=(1, 1), padding='same',
+                                       kernel_initializer=tf.truncated_normal_initializer(stddev=0.1, seed=1),
                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     output_layer7 = tf.layers.conv2d_transpose(conv_1x1_layer7, num_classes, 4, strides=(2, 2), padding='same',
+                                               kernel_initializer=tf.truncated_normal_initializer(stddev=0.1, seed=1),
                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     # manipulation for layer4
     conv_1x1_layer4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, strides=(1, 1), padding='same',
+                                       kernel_initializer=tf.truncated_normal_initializer(stddev=0.1, seed=1),
                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     # manipulation for layer3
     conv_1x1_layer3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, strides=(1, 1), padding='same',
+                                       kernel_initializer=tf.truncated_normal_initializer(stddev=0.1, seed=1),
                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     # skip connections
     input = tf.add(conv_1x1_layer4, output_layer7)
     input = tf.layers.conv2d_transpose(input, num_classes, 4, strides=(2, 2), padding='same',
+                                       kernel_initializer=tf.truncated_normal_initializer(stddev=0.1, seed=1),
                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     input = tf.add(input, conv_1x1_layer3)
     output = tf.layers.conv2d_transpose(input, num_classes, 16, strides=(8, 8), padding='same',
+                                        kernel_initializer=tf.truncated_normal_initializer(stddev=0.1, seed=1),
                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     return output
 tests.test_layers(layers)
